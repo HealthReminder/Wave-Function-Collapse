38c65877a796adc4e456dc9ec91e0253deff2d7e
Eduardo-PC <devugates@dronekids.com.br>
2020-09-05 23:37:42 -0700

    Initital cell collapse + Hyperposition
    The CollapseArray function collapses a initial cell. It is an optional parameter. It's position in the grid can be set or not. If not the first collapsed cell will be a random cell of the grid.

--- Wave-Collapse-Algorithm/Assets/Tester.cs
+++ Wave-Collapse-Algorithm/Assets/Tester.cs
@@ -48,36 +48,64 @@ public class Tester : MonoBehaviour
 
         log = "";
         for (int i = 0; i < unique.Count; i++)
-            log += "For pattern \n"+unique[i].GetValues()+"\n"+ unique[i].GetNeighbors(unique) + "\n-----------\n";
-        Debug.Log("<color=red> Neighbors of patterns: </color> " + log + "\n");
+            log += "For pattern \n" + unique[i].GetValues() + "\n" + unique[i].GetNeighbors(unique) + "\n-----------\n";
+        Debug.Log("<color=red> Neighbors of patterns: </color> " + log);
 
+        //Output an array of patterns of X size according to a pattern list
+        //Optional parameters to include a preset first cell
+        yield return CollapseArray(pattern_size*3, unique, 1);
 
-        //yield return CollapseArray(12, unique);
         Debug.Log("Finished test routine.");
         yield break;
     }
-    IEnumerator CollapseArray (int output_size, List<Pattern> all_patterns)
+    IEnumerator CollapseArray(int output_size, List<Pattern> all_patterns, int initial_pattern = -1, int initial_x = -1, int initial_y = -1)
     {
         if (all_patterns == null)
             Debug.LogError("Cannot collapse array with null patterns.");
         else if (all_patterns.Count <= 0)
             Debug.LogError("Cannot collapse array with no patterns.");
+
         //Setup useful lists and variables
         List<Vector2> infinite_cells = new List<Vector2>();
         int qtd_patterns = all_patterns.Count;
+
         //Setup the resulting array
         //Flag initial cells as cells with infinite possibilities
         collapsing_array = new List<int>[output_size][];
-        for (int i = 0; i < output_size; i++)
+        for (int y = 0; y < output_size; y++)
         {
-            collapsing_array[i] = new List<int>[output_size];
-            for (int o = 0; o < output_size; o++)
+            collapsing_array[y] = new List<int>[output_size];
+            for (int x = 0; x < output_size; x++)
             {
-                collapsing_array[o][i] = new List<int>();
-                infinite_cells.Add(new Vector2(o, i));
+                collapsing_array[y][x] = new List<int>();
+                infinite_cells.Add(new Vector2(y, x));
             }
         }
-        
+
+        #region WFC Algorithm
+
+        //Collapse first cell
+        if (initial_pattern != -1)
+        {
+            if (initial_x == -1)
+                initial_x = Random.Range(0, output_size);
+            if (initial_y == -1)
+                initial_y = Random.Range(0, output_size);
+            collapsing_array[initial_x][initial_y] = WFCCollapse.SetupHyperposition(all_patterns);
+            WFCCollapse.CollapseCell(collapsing_array[initial_x][initial_y], all_patterns, initial_pattern);
+            Debug.Log("Collapse first cell.");
+        }
+
+        Debug.Log(collapsing_array.Length);
+        Debug.Log(collapsing_array[0].Length);
+        Debug.Log(collapsing_array[0][0].Count);
+
+
+        string log = "";
+        log += ReadArrayList(collapsing_array);
+        Debug.Log("<color=cyan> Collapsed list: </color> \n" + log);
+
+        #endregion
         yield break;
     }
     #endregion
@@ -88,7 +116,7 @@ public class Tester : MonoBehaviour
         int[][] result;
         List<int[]> lists = new List<int[]>();
         lists.Add(new int[4] { 1, 2, 1, 2 });
-        lists.Add(new int[4] { 4, 3, 4, 3});
+        lists.Add(new int[4] { 4, 3, 4, 3 });
         lists.Add(new int[4] { 1, 2, 1, 2 });
         lists.Add(new int[4] { 4, 3, 4, 3 });
         result = lists.ToArray();
@@ -96,28 +124,29 @@ public class Tester : MonoBehaviour
     }
     #endregion
     #region Debug
-    string ReadList (List<int> list)
+    string ReadList(List<int> list)
     {
         string log = "";
         for (int i = 0; i < list.Count; i++)
-            log += list[i] +" | ";
+            log += list[i] + " | ";
         return log;
     }
-    string ReadArrayList(List<int[][]> listarray)
+    string ReadArrayList(List<int>[][] arraylist)
     {
         string log = "";
-        for (int i = 0; i < listarray.Count; i++)
-        {
-            int s = listarray[i].Length;
-            for (int y = 0; y < s; y++)
-            {
-                for (int x = 0; x < s; x++)
-                {
-                    log += listarray[i][x][y];
-                }
-                log += "\n";
 
-            }
+        for (int y = 0; y < arraylist.Length; y++)
+        {
+            int s = arraylist[y].Length;
+            for (int x = 0; x < s; x++)
+            {
+                if (arraylist[y][x].Count <= 0)
+                    log += "X";
+                else if (arraylist[y][x].Count > 1)
+                    log += "O";
+                else if (arraylist[y][x].Count == 1)
+                    log += arraylist[y][x][0];
+                }
             log += "\n";
         }
         return log;
---
+++ Wave-Collapse-Algorithm/Assets/WaveFunctionCollapse/WFCCollapse.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace WaveFunctionCollapse
+{
+    public static class WFCCollapse
+    {
+        //COMECA A TRANSFORMAR ESSA COROTINA NA ROTINA DO TESTER
+        //AQUI SO TERA FUNCOES QUE RETORNAM TIPOS E NO ROUTINAS
+        public static void CollapseCell(List<int> possible_patterns, List<Pattern> patterns, int collapse_into = -1)
+        {
+            if (possible_patterns == null)
+                Debug.LogError("Cannot collapse a non-initialized cell");
+            else if (possible_patterns.Count == 0)
+                Debug.LogError("Cannot collapse a cell with no possible solutions");
+            else if (possible_patterns.Count == 1)
+                Debug.LogWarning("Should not collapse cell with already one solution");
+
+            if (collapse_into == -1)
+            {
+                possible_patterns.Clear();
+                possible_patterns.Add(collapse_into);
+            }
+            else
+            {
+                int c = possible_patterns.Count;
+                List<int> r_indexes = new List<int>();
+                int current_pattern;
+                for (int i = 0; i < c; i++)
+                {
+                    current_pattern = possible_patterns[i];
+                    for (int o = 0; o < patterns[current_pattern].frequency; o++)
+                        r_indexes.Insert(Random.Range(0, r_indexes.Count), current_pattern);
+                }
+
+                //string log = "Cell with "+c+" possible indexes generated frequency list of: ";
+                //for (int h = 0; h < r_indexes.Count; h++)
+                //    log += r_indexes[h] + " ";
+                //Debug.Log(log);
+
+                int chosen_solution = r_indexes[Random.Range(0, r_indexes.Count)];
+                //Debug.Log("Collapsed cell into "+chosen_solution);
+                possible_patterns.Clear();
+                possible_patterns.Add(chosen_solution);
+                //Debug.Log("Collapsed cell");
+
+            }
+        }
+        public static List<int> SetupHyperposition(List<Pattern> patterns)
+        {
+            List<int>  p = new List<int>();
+            for (int i = 0; i < patterns.Count; i++)
+                p.Add(i);
+            return p;
+        }
+    }
+}
